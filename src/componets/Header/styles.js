ded9153deb9c2aac085b7a5cd67c97263f7b6ba2
Nicolas Tresoldi <nicolastresoldi@gmail.com>
2025-01-07 23:49:15 -0300

    alterado a cor do header

--- src/componets/Header/styles.js
+++ src/componets/Header/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   height: 150px;
   text-align: center;
-  background: teal;
+  background: rgb(87, 77, 192);
 `;
 
 export const Header = styled.h1``;
